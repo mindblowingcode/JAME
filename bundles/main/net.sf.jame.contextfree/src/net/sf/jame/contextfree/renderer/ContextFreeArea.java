18cec08f213d2eab70b16698af746f6b4eaa6c17
andrea <andrea@localhost>
2010-04-27 09:09:46 +0200

    contextfree was changed

---
+++ bundles/main/net.sf.jame.contextfree/src/net/sf/jame/contextfree/renderer/ContextFreeArea.java
@@ -0,0 +1,31 @@
+package net.sf.jame.contextfree.renderer;
+
+public class ContextFreeArea {
+	private float x;
+	private float y;
+	private float width;
+	private float height;
+	
+	public ContextFreeArea(float x, float y, float width, float height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public float getWidth() {
+		return width;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+}
