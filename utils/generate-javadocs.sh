ef0e1a3d3fb42695dc6dd1ded6d0b694b4417f69
Andrea Medeghini <andrea.medeghini@gmail.com>
2015-04-11 13:37:26 +0100

    Fixed script

--- utils/generate-javadocs.sh
+++ utils/generate-javadocs.sh
@@ -2,31 +2,29 @@ export ROOT=`pwd`/..
 
 classespath=''
 classespath=$classespath:$ROOT/maven/net.sf.jame.core/target/classes
-classespath=$classespath:$ROOT/maven/net.sf.jame.core.swing/target/classes
 classespath=$classespath:$ROOT/maven/net.sf.jame.twister/target/classes
-classespath=$classespath:$ROOT/maven/net.sf.jame.twister.swing/target/classes
 classespath=$classespath:$ROOT/maven/net.sf.jame.mandelbrot/target/classes
-classespath=$classespath:$ROOT/maven/net.sf.jame.mandelbrot.swing/target/classes
 #classespath=$classespath:$ROOT/maven/net.sf.jame.contextfree/target/classes
-#classespath=$classespath:$ROOT/maven/net.sf.jame.contextfree.swing/target/classes
-classespath=$classespath:$ROOT/maven/net.sf.jame.twister/target/classes
+classespath=$classespath:$ROOT/maven/net.sf.jame.core.swing/target/classes
 classespath=$classespath:$ROOT/maven/net.sf.jame.twister.swing/target/classes
+classespath=$classespath:$ROOT/maven/net.sf.jame.mandelbrot.swing/target/classes
+#classespath=$classespath:$ROOT/maven/net.sf.jame.contextfree.swing/target/classes
 classespath=$classespath:$ROOT/maven/net.sf.jame.queue/target/classes
 classespath=$classespath:$ROOT/maven/net.sf.jame.maven/target/classes
 classespath=$classespath:$ROOT/maven/net.sf.jame.devtools/target/classes
 
 sourcespath=''
-sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.core/src
-sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.core.swing/src
-sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.twister/src
-sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.twister.swing/src
-sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.mandelbrot/src
-sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.mandelbrot.swing/src
-sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.queue/src
-sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.main/src
-sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.devtools/src
-#sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.contextfree/src
-#sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.contextfree.swing/src
+sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.core/src/main/java
+sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.twister/src/main/java
+sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.mandelbrot/src/main/java
+#sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.contextfree/src/main/java
+sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.core.swing/src/main/java
+sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.twister.swing/src/main/java
+sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.mandelbrot.swing/src/main/java
+#sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.contextfree.swing/src/main/java
+sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.queue/src/main/java
+sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.main/src/main/java
+sourcespath=$sourcespath:$ROOT/maven/net.sf.jame.devtools/src/main/java
 
 opts='-J-Xmx512M -author -version -use -d javadocs -subpackages net.sf.jame'
 
