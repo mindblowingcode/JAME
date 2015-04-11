820c1775133e451158d80e3215ba487ffd592f25
Andrea Medeghini <andrea.medeghini@gmail.com>
2015-04-11 13:32:32 +0100

    Updated copyright notice

--- utils/run-add-copyright.sh
+++ utils/run-add-copyright.sh
@@ -2,6 +2,6 @@ export ROOT=`pwd`/..
 
 java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright.txt $ROOT/maven
 
-java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright3.txt $ROOT/maven/net.sf.jame.mandelbrot/src/net/sf/jame/mandelbrot/renderer/BestXaosMandelbrotRenderer.java
-java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright3.txt $ROOT/maven/net.sf.jame.mandelbrot/src/net/sf/jame/mandelbrot/renderer/FastXaosMandelbrotRenderer.java
+java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright3.txt $ROOT/maven/net.sf.jame.mandelbrot/src/main/java/net/sf/jame/mandelbrot/renderer/BestXaosMandelbrotRenderer.java
+java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright3.txt $ROOT/maven/net.sf.jame.mandelbrot/src/main/java/net/sf/jame/mandelbrot/renderer/FastXaosMandelbrotRenderer.java
 
--- utils/run-change-copyright.sh
+++ utils/run-change-copyright.sh
@@ -1,3 +1,6 @@
 export ROOT=`pwd`/..
 
 java -classpath FileUtility.jar net.sf.jame.fileutility.ChangeCopyright copyright.txt $ROOT/maven
+
+java -classpath FileUtility.jar net.sf.jame.fileutility.ChangeCopyright copyright3.txt $ROOT/maven/net.sf.jame.mandelbrot/src/main/java/net/sf/jame/mandelbrot/renderer/BestXaosMandelbrotRenderer.java
+java -classpath FileUtility.jar net.sf.jame.fileutility.ChangeCopyright copyright3.txt $ROOT/maven/net.sf.jame.mandelbrot/src/main/java/net/sf/jame/mandelbrot/renderer/FastXaosMandelbrotRenderer.java
